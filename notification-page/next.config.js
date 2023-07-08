67c386b605ad3e3cc8f8c6373e6273fa7fbf4cbe
Dam Nghi <nghidam99@gmail.com>
2023-07-08 11:02:04 +0700

    Install common-js

--- notification-page/next.config.js
+++ notification-page/next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+    nextConfig,
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'styles')],
+      },
+}
